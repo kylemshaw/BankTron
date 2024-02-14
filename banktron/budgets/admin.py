6be6a798966cc504f16c600583e48bae1783452d
Kyle Shaw <kyleshaw@live.com>
2024-02-13 21:08:05 -0700

    register admin for budgets models

--- banktron/budgets/admin.py
+++ banktron/budgets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Budget, BudgetGroup, BudgetItem
+
+admin.site.register(Budget)
+admin.site.register(BudgetGroup)
+admin.site.register(BudgetItem)
